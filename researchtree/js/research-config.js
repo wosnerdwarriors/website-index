d15595dd4400013457dc0a57114e797e812285e8
Your Name <you@example.com>
2024-10-20 15:22:24 +0200

    research-config new format

--- researchtree/js/research-config.js
+++ researchtree/js/research-config.js
@@ -1,41 +1,207 @@
 const researchConfig = {
-	"Growth": [
+	"Growth": 
+	{
+		
+		"tooling-up-1":
 		{
-			"id": 1,
-			"name": "Tooling Up",
-			"level": 0,
-			"maxLevel": 6,
-			"prerequisites": []
+			"row": 1,
+			"name": "Tooling Up I",
+			"stat": "construction-speed",
+			"levels":
+			{
+				"0":
+				{
+					"stat-addition": 1.4,
+					"power": 500,
+					"cost":
+					{
+						"meat": 1000,
+						"wood": 4000,
+						"coal": 1322,
+						"iron": 52151,
+						"steel": 1332
+					},
+					"research-time": 5021
+				},
+				"1":
+				{
+					"stat-addition": 1.6,
+					"power": 600,
+					"cost":
+					{
+						"meat": 2000,
+						"wood": 5000,
+						"coal": 2322,
+						"iron": 62151,
+						"steel": 2332
+					},
+					"research-time": 7021
+				}
+			}
 		},
+		"ward-expansion-1":
 		{
-			"id": 2,
-			"name": "Ward Expansion",
-			"level": 0,
-			"maxLevel": 6,
-			"prerequisites": [1]
+			"row": 2,
+			"name": "Ward Expansion I",
+			"stat": "infirmary-capacity",
+			"levels":
+			{
+				"1":
+				{
+					"stat-addition": 1000,
+					"power": 500,
+					"cost":
+					{
+						"meat": 1000,
+						"wood": 4000,
+						"coal": 1322,
+						"iron": 52151,
+						"steel": 1332
+					},
+					"research-time": 5021
+				},
+				"2":
+				{
+					"stat-addition": 2000,
+					"power": 600,
+					"cost":
+					{
+						"meat": 2000,
+						"wood": 5000,
+						"coal": 2322,
+						"iron": 62151,
+						"steel": 2332
+					},
+					"research-time": 7021
+				},
+				"3":
+				{
+					"stat-addition": 3000,
+					"power": 600,
+					"cost":
+					{
+						"meat": 2000,
+						"wood": 5000,
+						"coal": 2322,
+						"iron": 62151,
+						"steel": 2332
+					},
+					"research-time": 7021
+				}
+			}
 		},
+		"camp-expansion-1":
 		{
-			"id": 3,
-			"name": "Camp Expansion",
-			"level": 0,
-			"maxLevel": 6,
-			"prerequisites": [1]
+			"row": 2,
+			"name": "Camp Expansion I",
+			"stat": "training-capacity",
+			"levels":
+			{
+				"1":
+				{
+					"stat-addition": 1000,
+					"power": 500,
+					"cost":
+					{
+						"meat": 1000,
+						"wood": 4000,
+						"coal": 1322,
+						"iron": 52151,
+						"steel": 1332
+					},
+					"research-time": 5021
+				},
+				"2":
+				{
+					"stat-addition": 2000,
+					"power": 600,
+					"cost":
+					{
+						"meat": 2000,
+						"wood": 5000,
+						"coal": 2322,
+						"iron": 62151,
+						"steel": 2332
+					},
+					"research-time": 7021
+				},
+				"3":
+				{
+					"stat-addition": 3000,
+					"power": 600,
+					"cost":
+					{
+						"meat": 2000,
+						"wood": 5000,
+						"coal": 2322,
+						"iron": 62151,
+						"steel": 2332
+					},
+					"research-time": 7021
+				}
+			}
 		},
+		"tool-enhancement-1":
 		{
-			"id": 4,
-			"name": "Tool Enhancement",
-			"level": 0,
-			"maxLevel": 6,
-			"prerequisites": [2, 3]
-		},
-		{
-			"id": 5,
-			"name": "Bandaging",
-			"level": 0,
-			"maxLevel": 6,
-			"prerequisites": [4]
+			"row": 3,
+			"name": "Tool Enhancement I",
+			"stat": "research-speed",
+			"levels":
+			{
+				"1":
+				{
+					"stat-addition": 1000,
+					"power": 500,
+					"cost":
+					{
+						"meat": 1000,
+						"wood": 4000,
+						"coal": 1322,
+						"iron": 52151,
+						"steel": 1332
+					},
+					"research-time": 5021
+				},
+				"2":
+				{
+					"stat-addition": 2000,
+					"power": 600,
+					"cost":
+					{
+						"meat": 2000,
+						"wood": 5000,
+						"coal": 2322,
+						"iron": 62151,
+						"steel": 2332
+					},
+					"research-time": 7021
+				},
+				"3":
+				{
+					"stat-addition": 3000,
+					"power": 600,
+					"cost":
+					{
+						"meat": 2000,
+						"wood": 5000,
+						"coal": 2322,
+						"iron": 62151,
+						"steel": 2332
+					},
+					"research-time": 7021
+				}
+			}
 		}
-	],
-	// Additional research trees like Economy and Battle will follow the same format
+
+	},
+	"Economy": 
+	{
+
+	},
+	"Battle": 
+	{
+
+	}
+
 };
 
