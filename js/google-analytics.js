f7992863673f7c5aaed066ef3942ef2da68e6edf
Your Name <you@example.com>
2024-10-21 15:34:28 +0200

    prevent against double loading

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -1,21 +1,31 @@
-// Function to check if "notrack" exists in the URL parameters
-function shouldTrack() {
-    const urlParams = new URLSearchParams(window.location.search);
-    return !urlParams.has('notrack'); // Returns true if notrack is not present
-}
+// Check if Google Tag Manager has already been initialized
+if (!window.gtmLoaded && window.location.search.indexOf('notrack=true') === -1) {
+    // Set a flag to ensure this script doesn't run multiple times
+    window.gtmLoaded = true;
 
-// Only run Google Tag Manager if "notrack" is not set
-if (shouldTrack()) {
     // Google Tag Manager (noscript)
-    const noscriptTag = document.createElement('noscript');
-    noscriptTag.innerHTML = '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-54D8C3P6" height="0" width="0" style="display:none;visibility:hidden"></iframe>';
-    document.body.appendChild(noscriptTag);
+    (function() {
+        var noscriptTag = document.createElement('noscript');
+        var iframeTag = document.createElement('iframe');
+        iframeTag.src = "https://www.googletagmanager.com/ns.html?id=GTM-54D8C3P6";
+        iframeTag.height = "0";
+        iframeTag.width = "0";
+        iframeTag.style.display = "none";
+        iframeTag.style.visibility = "hidden";
+        noscriptTag.appendChild(iframeTag);
+        document.body.insertBefore(noscriptTag, document.body.firstChild);
+    })();
 
     // Google Tag Manager (script)
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-54D8C3P6');
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-54D8C3P6');
 }
 
